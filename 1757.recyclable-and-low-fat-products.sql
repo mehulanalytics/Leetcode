20634b4f8a7bc13d6bc62e65173a344555d56bd4
leetcode <mehul.analytics@outlook.com>
2025-05-03 22:25:57 +0530

    added description and improve

--- 1757.recyclable-and-low-fat-products.sql
+++ 1757.recyclable-and-low-fat-products.sql
@@ -13,7 +13,25 @@
 product_id is the primary key (column with unique values) for this table.
 low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
 recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
- 
+ Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Only products 1 and 3 are both low fat and recyclable.
 
 Write a solution to find the ids of products that are both low fat and recyclable.
 
@@ -23,7 +41,7 @@ The result format is in the following example.*/
 
 -- @lc code=start
 select product_id from Products
-where low_fats="y" and recyclable="y";
+where low_fats='Y' and recyclable='Y';
 
 -- @lc code=end
 
