c29184b5f273bb47a953a253fc427f5140b06952
leetcode <mehul.analytics@outlook.com>
2025-05-04 15:53:30 +0530

    added

---
+++ 1378.replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,48 @@
+--
+-- @lc app=leetcode id=1378 lang=mysql
+--
+-- [1378] Replace Employee ID With The Unique Identifier
+--
+/*
+
+Category	Difficulty	Likes	Dislikes
+database	Easy (83.62%)	1654	138
+Tags
+Companies
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+
+The result format is in the following example.
+*/
+-- @lc code=start
+# Write your MySQL query statement below
+select unique_id,name from employees e left join EmployeeUNI eu
+on e.id=eu.id;
+-- @lc code=end
+
