8f901457d9c654b63e92b371e48ed43dceff995b
leetcode <mehul.analytics@outlook.com>
2025-05-06 17:43:59 +0530

    group by

---
+++ 596.classes-more-than-5-students.sql
@@ -0,0 +1,68 @@
+--
+-- @lc app=leetcode id=596 lang=mysql
+--
+-- [596] Classes More Than 5 Students
+--
+/*
+Classes More Than 5 Students
+Category	Difficulty	Likes	Dislikes
+database	Easy (60.26%)	1162	1079
+Tags
+Companies
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+*/
+
+-- @lc code=start
+# Write your MySQL query statement below
+select class from courses
+ group by class  having count(student)>=5;
+
+-- @lc code=end
+
